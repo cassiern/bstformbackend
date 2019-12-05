1e0d03d7424a64c3d7f1fdce696b8231e0bcc0e5
Cassie Nichols <cassienichols@macbooks-MacBook-Pro.local>
2019-12-05 12:52:32 -0700

    fixed CORS probelm

--- server.js
+++ server.js
@@ -2,18 +2,27 @@ const express = require('express');
 const app = express();
 require('./db/db');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 
 
 
 const postsController = require('./controllers/posts.js');
 const usersController = require('./controllers/users.js');
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+const corsOptions = {
+	origin: 'http://localhost:3000',
+	credentials: true,
+	optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+//allowing my react to talk to my server
 
 
 //url path to controllers
-app.use('/', postsController);
+app.use('/posts', postsController);
 app.use('/user', usersController);
 
 
