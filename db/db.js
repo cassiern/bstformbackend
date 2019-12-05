cdd8ceb9503fb3331cb605e767f3071acec51f1f
Cassie Nichols <cassienichols@macbooks-MacBook-Pro.local>
2019-12-05 08:57:53 -0700

    added folders and files. Also connected to MongoDB

---
+++ db/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost/bethel-code-test', {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useFindAndModify: false
+});
+
+mongoose.connection.on('connected', () => {
+	console.log('Mongoose is connected')
+});
+
+mongoose.connection.on('error', (err) => {
+	console.log(err, 'Mongoose failed to connect')
+});
+
+mongoose.connection.on('disconnected', () => {
+	console.log('Mongoose is disconnected')
+});
