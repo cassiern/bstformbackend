d93d3455bf9cd0fc50b2da9d00f6788656891f1a
Cassie Nichols <cassienichols@macbooks-MacBook-Pro.local>
2019-12-06 14:22:49 -0700

    trying to figure out problem with email coming from client to express...

--- models/users.js
+++ models/users.js
@@ -6,9 +6,11 @@ const UserSchema = new mongoose.Schema({
 	email: {
 		type: String,
 		unique: true,
+		required: true
 	},
 	password: {
 		type: String,
+		required: true
 	},
 	isLogged: Boolean
 });
